6746067c6db89b08eb4767fcff18faafa562f443
Sinekar Lintang <sinekarlintang@gmail.com>
2023-05-01 14:36:10 +0700

    decrypting anjay

---
+++ mqtt_esp32/decrypting/crypto_aead.h
@@ -0,0 +1,27 @@
+#define KAT_SUCCESS          0
+#define KAT_FILE_OPEN_ERROR -1
+#define KAT_DATA_ERROR      -3
+#define KAT_CRYPTO_FAILURE  -4
+
+#define MAX_MESSAGE_LENGTH         32
+#define MAX_ASSOCIATED_DATA_LENGTH 32
+#define MAX_KEY_LENGTH             32
+#define MAX_NONCE_LENGTH           24
+
+int crypto_aead_encrypt(
+	unsigned char *c, unsigned long long *clen,
+	const unsigned char *m, unsigned long long mlen,
+	const unsigned char *ad, unsigned long long adlen,
+	const unsigned char *nsec,
+	const unsigned char *npub,
+	const unsigned char *k
+);
+
+int crypto_aead_decrypt(
+	unsigned char *m, unsigned long long *mlen,
+	unsigned char *nsec,
+	const unsigned char *c, unsigned long long clen,
+	const unsigned char *ad, unsigned long long adlen,
+	const unsigned char *npub,
+	const unsigned char *k
+);
--- mqtt_esp32/decrypting/decrypting.ino
+++ mqtt_esp32/decrypting/decrypting.ino
@@ -19,25 +19,6 @@ extern "C"{
 #include "key_schedule.h"
 #include "PubSubClient.h"
 }
-/*
- Basic ESP8266 MQTT example
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "1sampai10" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-*/
 unsigned char ad[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E};
 unsigned char key[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
 unsigned char nonce[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
@@ -53,17 +34,16 @@ int ret_var;
 unsigned char ct[MAX_MESSAGE_LENGTH + CRYPTO_ABYTES];
 unsigned char msg2[MAX_MESSAGE_LENGTH];
 char msg1[96];
-unsigned long long  clen, mlen2;
+unsigned long long  clen, mlen2 =32;
 int func_ret, ret_val = KAT_SUCCESS;
 char input_string[MAX_MESSAGE_LENGTH * 2 + 1];
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
+#define MSG_BUFFER_SIZE  (50)
 unsigned char msg[MSG_BUFFER_SIZE] = "10.4";
 int value = 0;
-
 void setup_wifi() {
 
   delay(10);
@@ -80,6 +60,7 @@ void setup_wifi() {
   }
 
   randomSeed(micros());
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -87,29 +68,47 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  char new_char[32];
+  char decrypted[32];
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  Serial.print((int)length);
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    ct[i] = (char)payload[i];
+    Serial.print((char)ct[i]);
   }
+  Serial.print("\n");
+if ((func_ret = crypto_aead_decrypt(msg2, &mlen2, NULL, ct, 96, ad, 32, nonce, key)) != 0) {
+          
+            Serial.print("test");
+            ret_val = KAT_CRYPTO_FAILURE;
+          }
+   for (int i = 0; i < 32; i++) {
+        Serial.print((char)msg2[i]);
+        decrypted[i] = (char) msg2[i];
+      }
+  Serial.print(decrypted);
   Serial.println();
 }
 
 void reconnect() {
+  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP32Client-";
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
+    // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
+      // ... and resubscribe
       client.subscribe("testing");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -118,46 +117,15 @@ void reconnect() {
 void setup() {
   Serial.begin(115200);
   setup_wifi();
+  
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
-  char encrypted[96];
-  char decrypted[32];
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
-    unsigned long now = millis();
-    if (now - lastMsg > 2000) {
-      lastMsg = now;
-      ++value;
-      for (unsigned long long mlen = 0; (mlen <= MAX_MESSAGE_LENGTH) && (ret_val == KAT_SUCCESS); mlen++) {
-
-        for (unsigned long long adlen = 0; adlen <= MAX_ASSOCIATED_DATA_LENGTH; adlen++) {
-
-          if ((func_ret = crypto_aead_encrypt(ct, &clen, msg, mlen, ad, adlen, NULL, nonce, key)) != 0) {
-            ret_val = KAT_CRYPTO_FAILURE;
-            break;
-          }
-          if ((func_ret = crypto_aead_decrypt(msg2, &mlen2, NULL, ct, clen, ad, adlen, nonce, key)) != 0) {
-            ret_val = KAT_CRYPTO_FAILURE;
-            break;
-			    }
-        }
-      }
-      for (int i = 0; i < 32; i++) {
-        decrypted[i] = (char) msg2[i];
-      }
-      for (int i = 0; i < 48; i++) {
-        sprintf(&encrypted[i*2], "%02X", ct[i]);
-      }
-      snprintf (msg1, 100, "%s", encrypted);
-      //Serial.print("Publish message: ");
-      //Serial.print(msg1);
-      client.publish("testing", msg1);
-    }
-  
 }
